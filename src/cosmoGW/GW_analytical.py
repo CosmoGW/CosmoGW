ae4a2592d7e6b26297cda9b7c360d2de9eb85394
AlbertoRoper <alberto.roper.pol@gmail.com>
2025-07-11 15:26:18 +0200

    updated GW_analytical

--- src/cosmoGW/GW_analytical.py
+++ src/cosmoGW/GW_analytical.py
@@ -9,7 +9,7 @@ created in Dec. 2021
 Currently part of the cosmoGW code:
 
 https://github.com/cosmoGW/cosmoGW/
-https://github.com/cosmoGW/cosmoGW/blob/development/src/cosmoGW/GW_analytical.py
+https://github.com/cosmoGW/cosmoGW/blob/main/src/cosmoGW/GW_analytical.py
 
 Author: Alberto Roper Pol
 Created: 01/12/2021
@@ -40,17 +40,55 @@ early Universe: constant-in-time and coherent-decay models," in preparation
 
 import numpy as np
 import matplotlib.pyplot as plt
-import cosmoGW.plot_sets as plot_sets
 
-# reference values
-a_ref   = 4          # Batchelor spectrum k^4
-b_ref   = 5/3        # Kolmogorov spectrum k^(-5/3)
-alp_ref = 2          # reference smoothness of broken power-law transition
+### Reference slopes
+a_ref = 5      # Batchelor spectrum k^5
+b_ref = 2/3    # Kolmogorov spectrum k^(-2/3)
+alp_ref = 2    # reference smoothness of broken power-law transition
 
 ####### ANALYTICAL FUNCTIONS USED FOR A SMOOTHED BROKEN POWER LAW #######
 
+'''
+
+    RoperPol:2022iel and RoperPol:2025b consider a spectral function
+    defined such that the average squared field corresponds to
+
+    <v^2> ~ int zeta(K) dK,      in RoperPol:2022iel
+    <v^2> ~ int zeta(K) dlnK,    in RoperPol:2025b
+
+    The first convention can be chosen in the following functions if
+    dlogK is set to False, while the second one is assumed when dloK
+    is True
+
+'''
+
+def check_slopes(a, b, dlogk=True):
+
+    '''
+    Function that checks the slopes used in power spectra to ensure
+    that the integral over k or log k is converging.
+
+    Arguments:
+        a -- slope of the spectrum at low wave numbers, k^a
+        b -- slope of the spectrum at high wave numbers, k^(-b)
+        dlogk -- option to consider the spectral function per unit of k
+                 or unit of logarithm of k (see comment above)
+    '''
+
+    conv = True
+    if (dlogk):
+        if b < 0 or a < 0:
+            print('a and b have to be larger than 0')
+            conv = False
+    else:
+        if b < 1 or a < -1:
+            print('b has to be larger than 1 and a larger than -1')
+            conv = False
+
+    return conv
+
 def smoothed_bPL(k, A=1., a=a_ref, b=b_ref, kpeak=1., alp=alp_ref, norm=True,
-                 Omega=False, alpha2=False, piecewise=False):
+                 Omega=False, alpha2=False, piecewise=False, dlogk=True):
 
     """
     Function that returns the value of the smoothed broken power law (bPL) model
@@ -61,13 +99,11 @@ def smoothed_bPL(k, A=1., a=a_ref, b=b_ref, kpeak=1., alp=alp_ref, norm=True,
     where K = k/kpeak, c = 1 if a = 0 or c = abs(a) otherwise.
 
     This spectrum is defined such that kpeak is the correct position of
-    the peak and its maximum amplitude is given by A.
+    the peak and its maximum amplitude is given by A when norm is True.
 
     If norm is set to False, then the non-normalized spectrum is used:
 
         zeta (K) = A x K^a/(1 + K^(alp(a + b)))^(1/alp)
-
-    The function is only correctly defined when b > 0 and a + b >= 0
 
     Introduced in RoperPol:2022iel, see equation 6
     Main reference is RoperPol:2025b
@@ -84,18 +120,19 @@ def smoothed_bPL(k, A=1., a=a_ref, b=b_ref, kpeak=1., alp=alp_ref, norm=True,
                 kpeak and its maximum value is A
         Omega -- option to use the integrated energy density as the input A
         alpha2 -- option to use the alternative convention, such that the spectrum
-                  takes the form: zeta(K) ~ K^a/( b + c K^alpha )^((a + b)/alpha)
+                  takes the form: zeta(K) ~ K^a/( b + c K^alp )^((a + b)/alp)
         piecewise -- option to return a piecewise broken power law:
                      zeta(K) = K^a for K < 1, and K^(-b) for K > 1
-                    corresponding to the alpha -> infinity limit
+                     corresponding to the alpha -> infinity limit
+        dlogk -- option to consider the spectral function per unit of k
+                 or unit of logarithm of k (see comment above)
 
     Returns:
         spec -- spectrum array
     """
 
-    if b < max(0, -a):
-        print('b has to be larger than 0 and -a')
-        return 0*k**0
+    conv = check_slopes(a, b, dlogk=dlogk)
+    if not conv: return 0*k**0
 
     c = abs(a)
     if a == 0: c = 1
@@ -114,7 +151,8 @@ def smoothed_bPL(k, A=1., a=a_ref, b=b_ref, kpeak=1., alp=alp_ref, norm=True,
         else: spec = spec/(1 + K**alp2)**(1/alp)
 
     if Omega: spec = spec/kpeak/calA(a=a, b=b, alp=alp, norm=norm,
-                                     alpha2=alpha2, piecewise=piecewise)
+                                     alpha2=alpha2, piecewise=piecewise,
+                                     dlogk=dlogk)
 
     return spec
 
@@ -219,22 +257,26 @@ def Iabn(a=a_ref, b=b_ref, alp=alp_ref, n=0, norm=True, alpha2=False,
     return comp_beta*calI
 
 def calA(a=a_ref, b=b_ref, alp=alp_ref, norm=True, alpha2=False,
-         piecewise=False):
+         piecewise=False, dlogk=dlogk):
 
     '''
     Function that computes the parameter calA = Iab,0 that relates the
     peak and the integrated values of the smoothed_bPL spectrum
 
-    References are RoperPol:2022iel, equation 8, and RoperPol:2025b, appendix A
+    References are RoperPol:2022iel, equation 8 (for dlogk False)
+    and RoperPol:2025b, appendix A (for dlogk True)
 
-    Same arguments as Iabn function with n = 0
+    Same arguments as Iabn function with n = 0 or -1
     '''
 
-    return Iabn(a=a, b=b, alp=alp, n=0, norm=norm, alpha2=alpha2,
+    nn = 0
+    if dlogk: nn = -1
+
+    return Iabn(a=a, b=b, alp=alp, n=nn, norm=norm, alpha2=alpha2,
                 piecewise=piecewise)
 
 def calB(a=a_ref, b=b_ref, alp=alp_ref, n=1, norm=True, alpha2=False,
-         piecewise=False):
+         piecewise=False, dlogk=True):
 
     '''
     Function that computes the parameter calB = Iab;-1/Iab;0 that relates the
@@ -242,33 +284,105 @@ def calB(a=a_ref, b=b_ref, alp=alp_ref, n=1, norm=True, alpha2=False,
 
     Same arguments as Iabn function with n = -1 (Im1) and n = 0 (I0)
     Returns calB = (Im1/Im0)^n
+
+    Main reference is RoperPol:2025b, appendix A (for dlogk True)
     '''
 
-    Im1 = Iabn(a=a, b=b, alp=alp, n=-n, norm=norm, alpha2=alpha2,
+    nn = 0
+    if dlogk: nn = -1
+
+    Im1 = Iabn(a=a, b=b, alp=alp, n=-n + nn, norm=norm, alpha2=alpha2,
                piecewise=piecewise)
-    I0 = Iabn(a=a, b=b, alp=alp, n=0, norm=norm, alpha2=alpha2,
+    I0 = Iabn(a=a, b=b, alp=alp, n=nn, norm=norm, alpha2=alpha2,
               piecewise=piecewise)
     calB = (Im1/I0)**n
 
     return calB
 
+def zetam(a=a_ref, b=b_ref, alp=alp_ref, m=-1, norm=True, alpha2=False,
+         piecewise=False):
+
+    '''
+    Function that computes the amplitude at the peak of the function
+
+    K^m zeta(K)
+
+    It allows to relate spectral functions using different normalizations,
+    e.g., m = -1 allows to relate zeta used in RoperPol:2022iel with
+    zeta used in RoperPol:2025b
+
+    Main reference is RoperPol:2025b, appendix A
+
+    Arguments:
+        a -- slope of the spectrum at low wave numbers, k^a
+        b -- slope of the spectrum at high wave numbers, k^(-b)
+        kpeak -- spectral peak, i.e., position of the break from k^a to k^(-b)
+        alp -- smoothness of the transition from one power law to the other
+        m -- power of K^m used to compute new function K^m zeta(K)
+        norm -- option to normalize the spectrum such that its peak is located at
+                        kpeak and its maximum value is A
+        alpha2 -- option to use the alternative convention, such that the spectrum
+                          takes the form: zeta(K) ~ K^a/( b + c K^alp )^((a + b)/alp)
+        piecewise -- option to return a piecewise broken power law:
+                             zeta(K) = K^a for K < 1, and K^(-b) for K > 1
+                             corresponding to the alpha -> infinity limit
+
+    Returns:
+        zetam -- amplitude of new function K^m zeta(K)
+    '''
+
+    if piecewise: return 1.
+
+    if alpha2: alp2 = alp
+    else: alp2 = alp*(a + b)
+    zetam = ((a + m)/a)**((a + m)/alp2)*((b - m)/b)**((b - m)/alp2)
+
+    return zetam
+
+def Kstarm(a=a_ref, b=b_ref, alp=alp_ref, m=-1, norm=True, alpha2=False,
+         piecewise=False):
+
+    '''
+    Function that computes the location of the peak of the function K^m zeta(K)
+
+    Arguments are same as zetam
+    Returns:
+        Kstarm -- position of the peak of new function K^m zeta(K)
+    '''
+
+    if piecewise: return 1.
+
+    if alpha2: alp2 = alp
+    else: alp2 = alp*(a + b)
+    Kstarm = (b/a*(a + m)/(b - m))**(1./alp2)
+
+    return Kstarm
+
 def calC(a=a_ref, b=b_ref, alp=alp_ref, tp='vort', norm=True, alpha2=False,
-         piecewise=False, proj=True, q=1.):
+         piecewise=False):
 
     '''
     Function that computes the parameter calC that allows to
     compute the TT-projected stress spectrum by taking the convolution of the
     smoothed bPL spectra over k and tilde p = |k - p|.
 
-    It gives the spectrum of the stress of Gaussian vortical non-helical fields
-    as
+    It gives the value of the spectrum at K -> 0 limit as a prefactor
+    pref that depends on the type of source and an integral over
 
-    P_Pi (0) = 2 pi^2 EM*^2 calC / k*
+    zeta^2/P^4     when dlog is True (see appendix B of RoperPol:2025b)
+    zeta^2/P^2     when dlog is False (eq. 22 of RoperPol:2022iel for
+                     vortical fields and eq. 46 of RoperPol:2023dzg
+                     for compressional fields
 
-    References are:
-    a) RoperPol:2022iel, equation 22, for vortical
-    b) RoperPol:2023dzg, equation 46, for compressional
-    c) RoperPol:2025b (appendix A) for generic fields
+    The spectrum of the stresses is then normalized such that
+    (see appendix B of RoperPol:2025b)
+
+    EPi = K^3 Epeak^2 calC zetaPi
+
+    For the antisymmetric spectrum (helical) of the stresses
+    the normalization becomes
+
+    HPi = K^4 Hpeak Epeak calC zetaPi
 
     Arguments:
 
@@ -276,25 +390,27 @@ def calC(a=a_ref, b=b_ref, alp=alp_ref, tp='vort', norm=True, alpha2=False,
         b -- slope of the spectrum at high wave numbers, k^(-b)
         alp -- smoothness of the transition from one power law to the other
         tp -- type of sourcing field: 'vort' or 'comp' available
+        norm --
+        alpha2 --
+        piecewise --
     '''
 
-    if proj:
-        if tp   == 'vort': pref = 28/15
-        elif tp == 'comp': pref = 32/15
-        elif tp == 'mix':  pref = 16/5*q*(1 - q)
-        elif tp == 'hel':  pref = -4/3
-    else:
-        if tp   == 'vort': pref = 6
-        if tp   == 'comp': pref = 8
-        if tp   == 'mix':  pref = 8*q*(1 - q)
-        if tp   == 'hel':  pref = -2
+    pref = 1.
 
-    if tp not in ['vort', 'comp', 'mix', 'hel']:
+    if tp   == 'vort': pref = 28/15
+    elif tp == 'comp': pref = 32/15
+    elif tp == 'mix':  pref = 16/5
+    elif tp == 'hel':  pref = 1/3
+
+    if tp not in ['vort', 'comp', 'mix', 'hel', 'none']:
         print('tp has to be vortical (vort), compressional (comp),',
               'mixed (mix) or helical (hel)')
-        pref = 0.
+        print('returning pref = 1')
 
-    return pref*Iabn(a=a*2, b=b*2, alp=alp/2, n=-2, norm=norm,
+    nn = 0
+    if dlogk: nn = 2
+
+    return pref*Iabn(a=a*2, b=b*2, alp=alp/2, n=-2-nn, norm=norm,
                      alpha2=alpha2, piecewise=piecewise)
 
 ###### ANALYTICAL TEMPLATE USED FOR A DOUBLE SMOOTHED BROKEN POWER LAW ######
