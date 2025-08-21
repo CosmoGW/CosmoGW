fad1a40eaf25a42d32e04aec33b93239ec75bf12
AlbertoRoper <alberto.roper.pol@gmail.com>
2025-08-21 20:32:28 +0200

    updated __init__ file to remove comments

--- src/cosmoGW/__init__.py
+++ src/cosmoGW/__init__.py
@@ -1,27 +1,3 @@
-# def find_path():
-
-#     ## find the directory where cosmoGW is installed within sys.path
-#     import sys
-#     import os
-#     found = False
-#     paths = sys.path
-#     pth = 'unkown'
-#     for path in paths:
-#       subdirs = os.walk(path)
-#       subdirs = list(subdirs)
-#       for j in subdirs:
-#         if not 'test' in j[0]:
-#             if 'cosmoGW' in j[0]:
-#                 pth = j[0]
-#                 found = True
-#                 break
-#       if found: break
-#     if pth == 'unkown': print('cosmoGW cannot be found, make sure you are',
-#                              ' not using an environment named cosmoGW')
-#     return pth + '/'
-
-# COSMOGW_HOME = find_path()
-
 import os
 import importlib.util
 
@@ -69,11 +45,3 @@ __all__ = [
     "COSMOGW_HOME"
 ]
 
-# # take values from higgsless dataset
-# import pandas as pd
-# dirr = COSMOGW_HOME + 'resources/higgsless/parameters_fit_sims.csv'
-# try:
-# 	df = pd.read_csv(dirr)
-# 	del(df)
-# except:  print('cosmoGW cannot be found, make sure you are',
-#                              ' not using an environment named cosmoGW')
